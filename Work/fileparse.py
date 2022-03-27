be83f66f94d758a36407b5e130284582069cce31
quantfor <intuitionwith@gmail.com>
2022-03-27 12:00:23 +0900

    3.3 오류 검사

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,10 +4,14 @@
 import csv
 
 
-def parse_csv(filename: str, select: bool = None, types: list = [], has_headers: bool = True, delimiter: str = '') -> list:
+def parse_csv(filename: str, select: bool = None, types: list = [], has_headers: bool = True, delimiter: str = '', silence_errors: bool = False) -> list:
     """
     CSV 파일을 파싱해 레코드의 목록을 생성
     """
+    
+    if select and not(has_headers):
+        raise RuntimeError("select argument requires column headers")
+    
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter) if delimiter else csv.reader(f)
         
@@ -23,22 +27,27 @@ def parse_csv(filename: str, select: bool = None, types: list = [], has_headers:
             indices = []
 
         records = []
-        for row in rows:
+        for i, row in enumerate(rows):
             if not row:
                 continue
-
+     
             if indices:
                 row = [row[index] for index in indices]
 
             if types:
-                row = [func(val) for func, val in zip(types, row)]
-            
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not(silence_errors):
+                        print(f"Row {i+1}: Couldn't convert {row}")
+                        print(f"Row {i+1}: Reason {e}")
+                
             if has_headers:
                 record = dict(zip(headers, row))
-               
+            
             else:
                 record = tuple(row)
-                
+
             records.append(record)
 
         return records
@@ -61,3 +70,9 @@ if __name__ == "__main__":
     print('')
     portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], delimiter=' ')
     print(portfolio)
+    
+    portfolio2 = parse_csv('Data/missing_origin.csv', types=[str, int, float])
+    print(portfolio2)
+    
+    portfolio = parse_csv('Data/missing.csv', types=[str,int,float], silence_errors=True)
+    print(portfolio2)
