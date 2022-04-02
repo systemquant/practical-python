3a6e53b960274996d42874a1ef8df4f119aa45f0
quantfor <intuitionwith@gmail.com>
2022-04-02 10:51:47 +0900

    3.12

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
-#
-# Exercise 1.5
+
+height = 100
+bounce = 1
+while bounce <= 10:
+    height = height * (3/5)
+    print(bounce, round(height, 4))
+    bounce += 1
--- Work/report.py
+++ Work/report.py
@@ -2,34 +2,21 @@
 #
 # Exercise 2.4
 
-import csv
+#import csv
+import fileparse
 from pprint import pprint
 
 
 def read_portfolio(filename):
-    portfolio = []
+    portfolio = fileparse.parse_csv(
+        filename, select=['name', 'shares', 'price'], types=[str, int, float])
 
-    with open(filename, "rt") as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-
-        for row in rows:
-            # holding = ( row[0], int(row[1]), float(row[2]) )
-            holding = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
-            portfolio.append(holding)
-
-        return portfolio
+    return portfolio
 
 
 def read_prices(filename):
-    prices = {}
-    f = open("Data/prices.csv", "r")
-    rows = csv.reader(f)
-    for row in rows:
-        try:
-            prices[row[0]] = float(row[1])
-        except:
-            pass
+    prices = dict(fileparse.parse_csv(
+        filename, types=[str, float], has_headers=False))
 
     return prices
 
