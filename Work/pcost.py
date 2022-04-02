d520f6b5d99f716e9c98c0bce6877d990580cf3d
quantfor <intuitionwith@gmail.com>
2022-04-02 10:58:37 +0900

    3.4 모듈 끝

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,40 +1,31 @@
-# pcost.py with csv module
-import csv, sys
-import enum
+# pcost.py
+import sys
+import report
+
 
 def portfolio_cost(filename):
 
     total_cost = 0.0
+    portfolio = report.read_portfolio(filename)
+    
+    for rowno, record in enumerate(portfolio):
+        try:
+            nshares = int(record['shares'])
+            price = float(record['price'])
+        except ValueError as e:
+            print(f'Row {rowno}: Bad line: {record}, error {e}')
 
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
+        else:
+            total_cost += nshares * price
 
-        headers = next(rows)
-
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-            except ValueError as e:
-                print(f'Row {rowno}: Bad row: {row}, error {e}')
-            
-            else:
-                total_cost += nshares * price
-
-        return total_cost
-
-                
-
-
+    return total_cost
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         filename = sys.argv[1]
     else:
         filename = 'Data/portfolio.csv'
-    
+
     cost = portfolio_cost(filename)
 
     print('Total cost', cost)
