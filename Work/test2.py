d2301e11b2196c43b4cd0d52022e069742c3084c
quantfor <intuitionwith@gmail.com>
2022-05-01 21:12:02 +0900

    6.2 이터레이션을 커스터마이징

--- Work/test2.py
+++ Work/test2.py
@@ -1,3 +1,6 @@
+from tkinter import END
+
+
 def custom_iter(obj):
     _iter = obj.__iter__()
     while True:
@@ -6,6 +9,18 @@ def custom_iter(obj):
         except StopIteration:
             break
 
-with open("Data/portfolio.csv") as f:
-    custom_iter(f)
+def filematch(filename, substr):
+    with open(filename, 'r') as f:
+        for line in f:
+            if substr in line:
+                yield line
+
+if __name__ == '__main__':
+    # with open("Data/portfolio.csv") as f:
+    #     custom_iter(f)
+        
+    for line in filematch("Data/portfolio.csv", 'IBM'):
+        print(line, end='')
+    
+
 
